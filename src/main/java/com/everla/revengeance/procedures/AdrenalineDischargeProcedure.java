fbe444a3d0db5313b5aee00a041b03d3d185f2cd
EverlaPrismerAya <everlaprismeraya@gmail.com>
2025-06-09 17:27:57 +0800

    fixed sound issue when adrenaline is charging

--- src/main/java/com/everla/revengeance/procedures/AdrenalineDischargeProcedure.java
+++ src/main/java/com/everla/revengeance/procedures/AdrenalineDischargeProcedure.java
@@ -34,11 +34,15 @@ public class AdrenalineDischargeProcedure {
 		if (entity instanceof LivingEntity _livingEntity0 && _livingEntity0.getAttributes().hasAttribute(RevengeanceModAttributes.ADRENALINE_LEVEL)) {
 			if (entity instanceof LivingEntity _livingEntity1 && _livingEntity1.getAttributes().hasAttribute(RevengeanceModAttributes.ADRENALINE_LEVEL))
 				_livingEntity1.getAttribute(RevengeanceModAttributes.ADRENALINE_LEVEL).setBaseValue(0);
-			{
-				Entity _ent = entity;
-				if (!_ent.level().isClientSide() && _ent.getServer() != null) {
-					_ent.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, _ent.position(), _ent.getRotationVector(), _ent.level() instanceof ServerLevel ? (ServerLevel) _ent.level() : null, 4,
-							_ent.getName().getString(), _ent.getDisplayName(), _ent.level().getServer(), _ent), "playsound revengeance:adrenaline_charge_break player @s");
+			if ((entity instanceof LivingEntity _livingEntity2 && _livingEntity2.getAttributes().hasAttribute(RevengeanceModAttributes.ADRENALINE_LEVEL)
+					? _livingEntity2.getAttribute(RevengeanceModAttributes.ADRENALINE_LEVEL).getValue()
+					: 0) == 10000) {
+				{
+					Entity _ent = entity;
+					if (!_ent.level().isClientSide() && _ent.getServer() != null) {
+						_ent.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, _ent.position(), _ent.getRotationVector(), _ent.level() instanceof ServerLevel ? (ServerLevel) _ent.level() : null, 4,
+								_ent.getName().getString(), _ent.getDisplayName(), _ent.level().getServer(), _ent), "playsound revengeance:adrenaline_charge_break player @s");
+					}
 				}
 			}
 		}
